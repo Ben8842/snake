856176da42d6a1ce1e668c4358f6192cf3ae37bb
Ben8842 <z87e99b44r55a@gmail.com>
2021-03-13 14:22:11 -0500

    building the structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,90 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pathO: [[0, 0]],
+      flagStart: false,
+    };
+  }
+
+  renderSquare(x, y) {
+    var { pathO, flagStart } = this.state;
+
+    function randomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+  }
+
+  begin() {
+    console.log("begin");
+  }
+
+  render() {
+    var { pathO, flagStart } = this.state;
+    const elementS = [];
+    const elementZ = [];
+    const screenSize = 25;
+
+    const aray = [];
+    const bray = [];
+    var a;
+    var b;
+    //control section
+
+    var x;
+    var y;
+    for (y = 0; y < screenSize; y++) {
+      for (x = 0; x < screenSize; x++) {
+        elementS.push(this.renderSquare(x, y));
+      }
+      elementZ.push(
+        <div className="newLine">
+          {elementS.map((value, index) => {
+            return value;
+          })}
+        </div>
+      );
+      for (x = 0; x < screenSize; x++) {
+        elementS.pop();
+      }
+    }
+
+    const startButton = (
+      <div>
+        <button id="largebutton" onClick={() => this.begin()}>
+          Click to Start
+        </button>
+      </div>
+    );
+
+    const entireThingz = (
+      <div className="entireThing">
+        {startButton}
+
+        <div className="directionLand">
+          {bray.map((value, index) => {
+            return <span key={index}>{value}</span>;
+          })}
+        </div>
+        <div id="mazeSpot">
+          {elementZ.map((value, index) => {
+            return <span key={index}>{value}</span>;
+          })}
+        </div>
+      </div>
+    );
+
+    return (
+      <div>
+        Hello World
+        <p>More!</p>
+        {entireThingz}
+      </div>
+    );
+  }
 }
 
 export default App;
